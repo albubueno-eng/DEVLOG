e84eb6e99d271da37ff57abcac84aa82bc1b0aa0
albubueno-eng <albu.bueno@gmail.com>
2026-05-03 13:30:45 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *  - Versão bumped → cache antigo é deletado automaticamente
  */
 
-const CACHE_VERSION = 'v8.9';
+const CACHE_VERSION = 'v9';
 const CACHE_NAME = 'godmode-' + CACHE_VERSION;
 
 const STATIC_ASSETS = [
