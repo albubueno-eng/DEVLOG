b38882829982237ea35569486aa9cd0ed79acd6e
albubueno-eng <albu.bueno@gmail.com>
2026-05-02 21:09:50 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Motor do PWA (Focado em Performance e Atualização em Tempo Real)
 
-const CACHE_NAME = 'god-mode-v4.6';
+const CACHE_NAME = 'god-mode-v4.7';
 
 // Força o Service Worker a instalar imediatamente
 self.addEventListener('install', (event) => {
